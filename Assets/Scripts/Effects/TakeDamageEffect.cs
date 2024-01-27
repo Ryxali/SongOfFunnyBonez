204e3d24d2c27a0096109c2bf38f1b0cd87850eb
Niklas Lindblad <ryxali@gmail.com>
2024-01-27 18:55:50 +0100

    Added TakeDamage & Die Event and renamed some events

---
+++ Assets/Scripts/Effects/TakeDamageEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TakeDamageEffect : MonoEffect
+{
+    [SerializeField]
+    private MetronomeEvent deathEvent;
+
+    [SerializeField]
+    private SpriteRenderer[] hearts;
+    [SerializeField]
+    private Color damagedColor;
+
+    private int lives;
+
+    private void Awake()
+    {
+        lives = hearts.Length;
+    }
+
+    [ContextMenu("Trigger Damage")]
+    protected override void OnEvent()
+    {
+        for (int i = lives; i < hearts.Length; i++)
+            hearts[i].color = damagedColor;
+        lives--;
+        if (lives <= 0 && deathEvent)
+            deathEvent.Trigger();
+    }
+}
