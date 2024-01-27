ae4271c6d2799ab1bc75a1281db31e57fcde3423
Niklas Lindblad <ryxali@gmail.com>
2024-01-27 23:26:52 +0100

    Camera panning

--- Assets/Scripts/Effects/PanCameraEffect.cs
+++ Assets/Scripts/Effects/PanCameraEffect.cs
@@ -13,6 +13,8 @@ public class PanCameraEffect : MonoEffect
     private Vector3 pan;
     [SerializeField]
     private Vector3 offset;
+    [SerializeField]
+    private float maxVelocity;
     protected override void OnEvent()
     {
         isPanning = true;
@@ -22,7 +24,7 @@ public class PanCameraEffect : MonoEffect
     {
         if (isPanning)
         {
-            cameraTarget.position = Vector3.SmoothDamp(cameraTarget.position, panTarget.position + offset, ref pan, 1f, Vector3.Distance(cameraTarget.position, panTarget.position));
+            cameraTarget.position = Vector3.SmoothDamp(cameraTarget.position, panTarget.position + offset, ref pan, 1f, Mathf.Min(Vector3.Distance(cameraTarget.position, panTarget.position), maxVelocity));
         }
     }
 }
